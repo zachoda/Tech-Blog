50549a046817d827794e24db4fb17c3cf86921d3
Zach Oda <zjodachow@gmail.com>
2022-11-05 13:41:56 -0400

    fixed routes to form

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -88,17 +88,6 @@ router.post('/', withAuth, (req, res) => {
   }
 });
 
-router.put('/upvote', withAuth, (req, res) => {
-  // custom static method created in models/Post.js
-  if (req.session) {
-    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-      .then(updatedVoteData => res.json(updatedVoteData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  }
-});
 
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
