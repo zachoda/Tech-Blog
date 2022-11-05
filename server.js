e372c75004e2558d9567b8530d4175a338ddbe92
Zach Oda <zjodachow@gmail.com>
2022-11-05 13:54:49 -0400

    CSS issue finally fixed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 app.use(session(sess));
 // turn on routes
 app.use(routes);
